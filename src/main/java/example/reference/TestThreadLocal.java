e478832df2b04992ab021cc1039e4090e2e4a6c1
LiuZijian <lzj@123>
2020-06-15 14:25:02 +0800

    add

--- src/main/java/example/reference/TestThreadLocal.java
+++ src/main/java/example/reference/TestThreadLocal.java
@@ -2,9 +2,11 @@ package example.reference;
 
 public class TestThreadLocal {
 
-    private static ThreadLocal<M> threadLocal = new ThreadLocal<>();
+    private static ThreadLocal<M> threadLocal1 = new ThreadLocal<>();
     
     public static void main(String[] args) {
-        
+        threadLocal1.set(new M());
+        System.out.println(threadLocal1.get());
+    
     }
 }
